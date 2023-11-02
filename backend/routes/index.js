01ac9d33c3a1a2e614ae10bd98c47570b7c62658
Julia Khabibova <julia.khabibova@mail.ru>
2023-11-02 21:25:39 +0500

    burger menu

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,12 +2,6 @@ const router = require('express').Router();
 const NotFoundError = require('../errors/NotFoundError');
 const auth = require('../middlewares/auth');
 
-router.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 router.use('/signin', require('./signin'));
 router.use('/signup', require('./signup'));
 
@@ -18,4 +12,5 @@ router.use('/cards', require('./cards'));
 router.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
+
 module.exports = router;
